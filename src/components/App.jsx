11921d8afc3b03f705cf193731e678a3b1423580
Marlena Dulęba <marlena_duleba@o2.pl>
2023-03-25 17:58:53 +0100

    main structure added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import Container from "./Container/Container";
+import ImageFinder from "./ImageFinder/ImageFinder";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <ImageFinder />
+    </Container>
   );
 };
---
+++ src/components/ImageFinder/ImageFinder.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+export default class ImageFinder extends Component {
+  state = {};
+
+  render() {
+    return <div>ImageFinder</div>;
+  }
+}
