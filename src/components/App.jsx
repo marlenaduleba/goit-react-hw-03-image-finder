05e4cb84c258c9d1864099ee366132d9c7319a52
Marlena Dulęba <marlena_duleba@o2.pl>
2023-03-26 09:53:01 +0200

    import corrected

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import Container from "./Container/Container";
+import { Container } from "./Container/Container";
 import ImageFinder from "./ImageFinder/ImageFinder";
 
 export const App = () => {
